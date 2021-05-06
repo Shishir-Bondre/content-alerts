008e6e302561e7df21d38f65c1ad23f62a36328b
Shishir Bondre <shishir.bondre@snapengage.com>
2021-05-06 12:20:07 +0530

    v8

--- tracker/settings.py
+++ tracker/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # settings/base.py
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
