9c5b56724e9953c39e5a4bd7793f245cbb90aee4
Shishir Bondre <shishir.bondre@snapengage.com>
2021-05-06 12:37:09 +0530

    v14

--- tracker/settings.py
+++ tracker/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # settings/base.py
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
