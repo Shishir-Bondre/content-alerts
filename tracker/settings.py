37be34c781338ef3968e1257f5c0d1148bb022e5
Shishir Bondre <bondreshishir@gmail.com>
2021-05-11 11:29:50 +0530

    Update settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -152,5 +152,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "timetobookyourslot@gmail.com"
-EMAIL_HOST_PASSWORD = "qazwsx@123"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
